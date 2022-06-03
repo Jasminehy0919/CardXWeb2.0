bf6353be35c6c4818f0a25d72a513001a12a2694
Jasminehy0919 <65463499+Jasminehy0919@users.noreply.github.com>
2022-06-03 11:54:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,19 @@
 $(".download-button").hover(function() {
   $(".title-QR").addClass("shake_effect");
-  setTimeout(function(){
+  setTimeout(function() {
     $(".title-QR").removeClass("shake_effect");
-  },500)
+  }, 500)
 });
 
 
 
-
-
-
-
-
-//
-//
-// var vh = $(window).height(),
-//     vw = $(window).width(),
-//     triggerPoint = $('.trigger').offset().top;
-//
-// var firstStop = 0,
-//     endStop = 0;
-//
-// var colors = ['#ff0', '#f0f', '#0ff'];
-//
-// $(window).scroll(function()
-// {
-//   var scrollPos = $(document).scrollTop();
-//
-//   if(scrollPos < triggerPoint)
-//   {
-//     firstStop = scrollPos / vh * 75;
-//     endStop = 100 - (scrollPos / vh * 75);
-//
-//     $('.container-test').css({
-//       'background': 'linear-gradient(135deg, ' + colors[1] + ' ' + firstStop + '%, ' + colors[0] + ' ' + firstStop + '%, ' + colors[0] + ' ' + endStop + '%, ' + colors[1] + ' ' + endStop + '%)'
-//     });
-//   }
-//   else
-//   {
-//     endStop = (scrollPos - triggerPoint) / vh * 75;
-//     firstStop = 100 - (scrollPos - triggerPoint) / vh * 75;
-//
-//     $('.container-test').css({
-//       'background': 'linear-gradient(225deg, ' + colors[1] + ' ' + firstStop + '%, ' + colors[2] + ' ' + firstStop + '%, ' + colors[2] + ' ' + endStop + '%, ' + colors[1] + ' ' + endStop + '%)'
-//     });
-//   }
-//
-// });
-
-
-
-$(document).scroll(function(){
-   if ($(document).scrollTop() > 320 ) {
-     $('.main').addClass('active');
-     $('.reveal-main').addClass('activetwo');
-   }
-
-  else {
+$(document).scroll(function() {
+  if ($(document).scrollTop() > 0) {
+    $('.main').addClass('active');
+    $('.reveal-main').addClass('activetwo');
+  } else {
     $('.main').removeClass('active');
-         $('.reveal-main').removeClass('activetwo');
+    $('.reveal-main').removeClass('activetwo');
   }
 
 });
